be73ecaa6fd2648f1b868c63dd870f3cc7e36434
tmtuan684 <tuan.tran24@rmit.edu.vn>
2025-08-01 17:21:29 +0700

    Added week 5 practices

--- practice/week05/dbsupport.js
+++ practice/week05/dbsupport.js
@@ -1,5 +1,6 @@
 function getConnString(db) {
-    return `mongodb+srv://tmtuan:mypassword@cosc3060.rqcqojz.mongodb.net/${db}?retryWrites=true&w=majority&appName=cosc3060`;
+    // Change the connection string with connection string to your MongoDB altas
+    return `mongodb+srv://<your DB account>:<your DB account password>@cosc3060.rqcqojz.mongodb.net/${db}?retryWrites=true&w=majority&appName=cosc3060`;
 }
 
 module.exports = { getConnString };
